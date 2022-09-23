ab4eed98c583a43917f7f8272644d6eee2191bf3
Jenny Spadoni <jennyspadoni@gmail.com>
2022-09-23 13:07:49 -0700

    Added card style formating to trip briefs

--- controllers/pastTrips.js
+++ controllers/pastTrips.js
@@ -9,5 +9,14 @@ module.exports = {
         console.log(err);
       }
   },
+  getPost: async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      const comments = await Comment.find({post: req.params.id}).sort({createdAt: 'desc'}).lean()
+      res.render("post.ejs", { post: post, user: req.user, comments: comments});
+    } catch (err) {
+      console.log(err);
+    }
+  },
   };
   
