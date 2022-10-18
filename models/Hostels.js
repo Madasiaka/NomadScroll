10a7e6e296dcbf623b0311b6b57e1a614b9f35bd
Jenny Spadoni <jennyspadoni@gmail.com>
2022-10-17 20:38:07 -0700

    mapquest is out, google maps is in (all my homies like google maps)

--- models/Hostels.js
+++ models/Hostels.js
@@ -1,196 +1,196 @@
-{
-  "type": "FeatureCollection",
-  "features":
-      [{
-          "type": "Feature",
-          "properties": {
-              "name": "Vivo Escondido",
-          },
-          "geometry": {
-              "type": "Point",
-              "coordinates": [
-                  -97.0788323879242,
-                  15.862872024938756
-              ]
-          }
-      },
-      {
-          "type": "Feature",
-          "properties": {
-              "name": "Shalom"
-          },
-          "geometry": {
-              "type": "Point",
-              "coordinates": [
-                  -97.07559764385223,
-                  15.861654229715937
-              ]
-          }
-      },
-      {
-          "type": "Feature",
-          "properties": {
-              "name": "Zion Hostel & Bungalows"
-          },
-          "geometry": {
-              "type": "Point",
-              "coordinates": [
-                  -97.0846152305603,
-                  15.864838026280848
-              ]
-          }
-      },
-      {
-          "type": "Feature",
-          "properties": {
-              "name": "Napelero Hostel"
-          },
-          "geometry": {
-              "type": "Point",
-              "coordinates": [
-                  -97.07742691040039,
-                  15.86873901154986
-              ]
-          }
-      },
-      {
-          "type": "Feature",
-          "properties": {
-              "name": "Tower Bridge Hostel"
-          },
-          "geometry": {
-              "type": "Point",
-              "coordinates": [
-                  -97.08043098449707,
-                  15.868914450912325
-              ]
-          }
-      },
-      {
-          "type": "Feature",
-          "properties": {
-              "name": "Chez Gomez"
-          },
-          "geometry": {
-              "type": "Point",
-              "coordinates": [
-                  -97.08293080329894,
-                  15.86598356153021
-              ]
-          }
-      },
-      {
-          "type": "Feature",
-          "properties": {
-              "name": "Casa Losodeli"
-          },
-          "geometry": {
-              "type": "Point",
-              "coordinates": [
-                  -97.07696557044983,
-                  15.862526295686672
-              ]
-          }
-      },
-      {
-          "type": "Feature",
-          "properties": {
-              "name": "Paziflora Hostel"
-          },
-          "geometry": {
-              "type": "Point",
-              "coordinates": [
-                  -97.07068920135498,
-                  15.861081450606223
-              ]
-          }
-      },
-      {
-          "type": "Feature",
-          "properties": { "name": "Casa Santaella" },
-          "geometry": {
-              "type": "Point",
-              "coordinates": [
-                  -97.070871591568,
-                  15.866623407214531
-              ]
-          }
-      },
-      {
-          "type": "Feature",
-          "properties": { "name": "Residencia Artistica" },
-          "geometry": {
-              "type": "Point",
-              "coordinates": [
-                  -97.07250237464905,
-                  15.867087810068297
-              ]
-          }
-      },
-      {
-          "type": "Feature",
-          "properties": {
-              "name": "Puerto Dreams"
-          },
-          "geometry": {
-              "type": "Point",
-              "coordinates": [
-                  -97.07285642623901,
-                  15.868790611378197
-              ]
-          }
-      },
-      {
-          "type": "Feature",
-          "properties": {
-              "name": "Casona Puerto Dreams"
-          },
-          "geometry": {
-              "type": "Point",
-              "coordinates": [
-                  -97.07966923713683,
-                  15.87009092269141
-              ]
-          }
-      },
-      {
-          "type": "Feature",
-          "properties": {
-              "name": "Selina Puerto Escondido"
-          },
-          "geometry": {
-              "type": "Point",
-              "coordinates": [
-                  -97.05803990364075,
-                  15.856850059036839
-              ]
-          }
-      },
-      {
-          "type": "Feature",
-          "properties": {
-              "name": "Hostel Mandala"
-          },
-          "geometry": {
-              "type": "Point",
-              "coordinates": [
-                  -97.05756783485413,
-                  15.856385632604704
-              ]
-          }
-      },
-      {
-          "type": "Feature",
-          "properties": {
-              "name": "Huitzilin Hostal"
-          },
-          "geometry": {
-              "type": "Point",
-              "coordinates": [
-                  -97.05261647701262,
-                  15.851823876156457
-              ]
-          }
-      }
-      ]
-}
+// {
+//   "type": "FeatureCollection",
+//   "features":
+//       [{
+//           "type": "Feature",
+//           "properties": {
+//               "name": "Vivo Escondido",
+//           },
+//           "geometry": {
+//               "type": "Point",
+//               "coordinates": [
+//                   -97.0788323879242,
+//                   15.862872024938756
+//               ]
+//           }
+//       },
+//       {
+//           "type": "Feature",
+//           "properties": {
+//               "name": "Shalom"
+//           },
+//           "geometry": {
+//               "type": "Point",
+//               "coordinates": [
+//                   -97.07559764385223,
+//                   15.861654229715937
+//               ]
+//           }
+//       },
+//       {
+//           "type": "Feature",
+//           "properties": {
+//               "name": "Zion Hostel & Bungalows"
+//           },
+//           "geometry": {
+//               "type": "Point",
+//               "coordinates": [
+//                   -97.0846152305603,
+//                   15.864838026280848
+//               ]
+//           }
+//       },
+//       {
+//           "type": "Feature",
+//           "properties": {
+//               "name": "Napelero Hostel"
+//           },
+//           "geometry": {
+//               "type": "Point",
+//               "coordinates": [
+//                   -97.07742691040039,
+//                   15.86873901154986
+//               ]
+//           }
+//       },
+//       {
+//           "type": "Feature",
+//           "properties": {
+//               "name": "Tower Bridge Hostel"
+//           },
+//           "geometry": {
+//               "type": "Point",
+//               "coordinates": [
+//                   -97.08043098449707,
+//                   15.868914450912325
+//               ]
+//           }
+//       },
+//       {
+//           "type": "Feature",
+//           "properties": {
+//               "name": "Chez Gomez"
+//           },
+//           "geometry": {
+//               "type": "Point",
+//               "coordinates": [
+//                   -97.08293080329894,
+//                   15.86598356153021
+//               ]
+//           }
+//       },
+//       {
+//           "type": "Feature",
+//           "properties": {
+//               "name": "Casa Losodeli"
+//           },
+//           "geometry": {
+//               "type": "Point",
+//               "coordinates": [
+//                   -97.07696557044983,
+//                   15.862526295686672
+//               ]
+//           }
+//       },
+//       {
+//           "type": "Feature",
+//           "properties": {
+//               "name": "Paziflora Hostel"
+//           },
+//           "geometry": {
+//               "type": "Point",
+//               "coordinates": [
+//                   -97.07068920135498,
+//                   15.861081450606223
+//               ]
+//           }
+//       },
+//       {
+//           "type": "Feature",
+//           "properties": { "name": "Casa Santaella" },
+//           "geometry": {
+//               "type": "Point",
+//               "coordinates": [
+//                   -97.070871591568,
+//                   15.866623407214531
+//               ]
+//           }
+//       },
+//       {
+//           "type": "Feature",
+//           "properties": { "name": "Residencia Artistica" },
+//           "geometry": {
+//               "type": "Point",
+//               "coordinates": [
+//                   -97.07250237464905,
+//                   15.867087810068297
+//               ]
+//           }
+//       },
+//       {
+//           "type": "Feature",
+//           "properties": {
+//               "name": "Puerto Dreams"
+//           },
+//           "geometry": {
+//               "type": "Point",
+//               "coordinates": [
+//                   -97.07285642623901,
+//                   15.868790611378197
+//               ]
+//           }
+//       },
+//       {
+//           "type": "Feature",
+//           "properties": {
+//               "name": "Casona Puerto Dreams"
+//           },
+//           "geometry": {
+//               "type": "Point",
+//               "coordinates": [
+//                   -97.07966923713683,
+//                   15.87009092269141
+//               ]
+//           }
+//       },
+//       {
+//           "type": "Feature",
+//           "properties": {
+//               "name": "Selina Puerto Escondido"
+//           },
+//           "geometry": {
+//               "type": "Point",
+//               "coordinates": [
+//                   -97.05803990364075,
+//                   15.856850059036839
+//               ]
+//           }
+//       },
+//       {
+//           "type": "Feature",
+//           "properties": {
+//               "name": "Hostel Mandala"
+//           },
+//           "geometry": {
+//               "type": "Point",
+//               "coordinates": [
+//                   -97.05756783485413,
+//                   15.856385632604704
+//               ]
+//           }
+//       },
+//       {
+//           "type": "Feature",
+//           "properties": {
+//               "name": "Huitzilin Hostal"
+//           },
+//           "geometry": {
+//               "type": "Point",
+//               "coordinates": [
+//                   -97.05261647701262,
+//                   15.851823876156457
+//               ]
+//           }
+//       }
+//       ]
+// }
